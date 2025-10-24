d2f062dd7d6dba9487abdfbc54f1dd3058947d09
MissHell <michaela.mickova111@gmail.com>
2025-10-24 15:19:43 +0200

    drag-n-drop

--- src/main/java/mickova/test/Main.java
+++ src/main/java/mickova/test/Main.java
@@ -5,22 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe/");
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://formy-project.herokuapp.com/keypress");
-        WebElement name = driver.findElement(By.id("name"));
-        name.click();
-        name.sendKeys("Myshelle Mickova");
-        WebElement button = driver.findElement(By.id("button"));
-        button.click();
+        driver.get("https://formy-project.herokuapp.com/dragdrop");
+        WebElement image = driver.findElement(By.id("image"));
+        WebElement box = driver.findElement(By.id("box"));
+
+        Thread.sleep(1000);
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(image, box).build().perform();
+        Thread.sleep(1000);
 
         driver.quit();
     }
