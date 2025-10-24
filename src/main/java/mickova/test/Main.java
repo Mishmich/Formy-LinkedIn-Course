c683caecd57402f35019f3065a90e573a68320dd
MissHell <michaela.mickova111@gmail.com>
2025-10-24 14:45:58 +0200

    alert-pop-up

--- src/main/java/mickova/test/Main.java
+++ src/main/java/mickova/test/Main.java
@@ -1,5 +1,6 @@
 package mickova.test;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,20 +9,20 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe/");
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://formy-project.herokuapp.com/keypress");
-        WebElement name = driver.findElement(By.id("name"));
-        name.click();
-        name.sendKeys("Myshelle Mickova");
-        WebElement button = driver.findElement(By.id("button"));
-        button.click();
+        driver.get("https://formy-project.herokuapp.com/switch-window");
+        WebElement alertBtn = driver.findElement(By.id("alert-button"));
+        alertBtn.click();
 
+        Alert alert = driver.switchTo().alert();
+        Thread.sleep(2000);  // Just to see the alert before accepting
+        alert.accept();
         driver.quit();
     }
 }
