f1912699eb853e46fd19eb82619557f6b412037f
MissHell <michaela.mickova111@gmail.com>
2025-10-25 11:19:03 +0200

    radio+checkbox

--- src/main/java/mickova/test/Main.java
+++ src/main/java/mickova/test/Main.java
@@ -8,20 +8,25 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         System.setProperty("webdriver.chrome.driver", "C:/Program Files/chromedriver-win64/chromedriver.exe/");
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://formy-project.herokuapp.com/keypress");
-        WebElement name = driver.findElement(By.id("name"));
-        name.click();
-        name.sendKeys("Myshelle Mickova");
-        WebElement button = driver.findElement(By.id("button"));
-        button.click();
-
+        driver.get("https://formy-project.herokuapp.com/radiobutton");
+        WebElement radioButton1 = driver.findElement(By.id("radio-button-1"));
+        Thread.sleep(1000);
+        radioButton1.click();
+        Thread.sleep(1000);
+        WebElement radioButton2 = driver.findElement(By.xpath("/html/body/div/div[2]/input"));
+        radioButton2.click();
+        Thread.sleep(1000);
+        driver.get("https://formy-project.herokuapp.com/checkbox");
+        WebElement check = driver.findElement(By.id("checkbox-3"));
+        Thread.sleep(1000);
+        check.click();
         driver.quit();
     }
 }
